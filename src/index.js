1a29f0b5818622bb6c15a1256f16bd4c2c093798
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2026-07-30 16:58:06 -0400

    Get weather from API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import "./styles.css";
 
-const body = document.querySelector("body");
-body.textContent = "Hello World";
+async function getWeather(location) {
+    try {
+        const weather = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=Y86AUV62SAQMW9QQ7DCTWZWMG`);
+        const json = weather.json();
+        console.log(json);
+    } catch (e) {
+        console.log(e);
+    }    
+}
 
-console.log("hello world");
+getWeather('Toronto');
